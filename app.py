68e0e450087b3cb2cdb313fd2f620b1dc29e3b4a
Luu Tran <thanhnientretrau73@gmail.com>
2025-11-29 18:56:24 +1100

    Initalize

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6007e9ad14c55d71cea2a4522259bb24868c2e61ff7e5b201561a62a0ca27dbf
+size 6954
---
+++ static/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa3fec24e9377aa06893b1ec5af3afad471026199df0334add16745c94f332d5
+size 6250
